5fe884711eb7d6e73443850217667fb9f1cef199
DariaKuznetsova <kds21082108@gmail.com>
2023-03-27 17:37:46 +0100

    third section, jquery plugins

---
+++ dist/js/main.js
@@ -0,0 +1,20 @@
+//scrollbar
+jQuery(document).ready(function(){
+    jQuery('.scrollbar-macosx').scrollbar();
+});
+
+
+//carousel slick
+$('.carousel1').slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1
+  });
+
+
+//carousel slick mini
+$('.carousel2').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1
+  });
